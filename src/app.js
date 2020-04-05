c63e581d4c2f46165a89943d3c8cd0786e95946a
polly-foz <polly.foz@gmail.com>
2020-04-06 01:48:28 +0800

    将button重构为vue单文件组件 让parcel用vue开发环境打包 使用`npx parcel index.html --no-cache`启动项目

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-// import Vue from 'vue'
-// new Vue({
-//     el: '#app',
-//     template: `
-//         <h1>hi vue</h1>
-//     `
-// })
-console.log("app.js")
+import Vue from 'vue'
+import Button from './button'
+
+Vue.component('pf-button',Button)
+
+new Vue({
+    el:'#app',
+})
---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="pf-button">按钮</button>
+</template>
+
+<script>
+    export default {
+
+    };
+</script>
+
+<style lang="scss" scoped>
+    .pf-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        /*取消按钮按下时的默认样式轮廓*/
+        &:focus {
+            outline: none;
+        }
+    }
+
+</style>
