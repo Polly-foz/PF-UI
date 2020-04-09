327970ce36533e0f21917a7753323cceaac08cb0
polly-foz <polly.foz@gmail.com>
2020-04-10 01:55:01 +0800

    add iconPosition to button

--- button.js
+++
@@ -1,6 +0,0 @@
-// button.js
-Vue.component('pf-button', {
-    template:`
-		<button class="pf-button">按钮</button>
-	`
-})
--- src/button.vue
+++ src/button.vue
@@ -1,31 +1,64 @@
 <template>
-    <button class="pf-button"><slot></slot></button>
+    <button class="pf-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content"><slot></slot></div>
+    </button>
 </template>
 
 <script>
     export default {
-
+        // props:['icon','iconPosition']
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(value){
+                    return value==='left'||value==='right'
+                }
+            }
+        }
     };
 </script>
 
 <style lang="scss" scoped>
-    .pf-button{
+    .pf-button {
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display:inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         &:hover {
             border-color: var(--border-color-hover);
         }
+
         &:active {
             background-color: var(--button-active-bg);
         }
+
         /*取消按钮按下时的默认样式轮廓*/
         &:focus {
             outline: none;
         }
+
+        > .icon{order:1; margin-reight:.3em;}
+        > .content{order:2;}
+
+        &.icon-right {
+            > .content {order:1;}
+            > .icon {order:2; margin-right:0; margin-left:.3em;}
+        }
     }
 
+    .icon {
+        width: 1em;
+        height: 1em;
+    }
 </style>
