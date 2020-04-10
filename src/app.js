e79b930f5da71638c5079b0ba0c8c70b54540445
polly-foz <polly.foz@gmail.com>
2020-04-10 16:18:50 +0800

    button-group.vue

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('pf-button',Button)
 Vue.component('pf-icon',Icon)
+Vue.component('pf-button-group',ButtonGroup)
 
 new Vue({
     el:'#app',
---
+++ src/button-group.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="pf-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "button-group",
+        mounted() {
+            for(let node of this.$el.children){
+                if(node.nodeName !== 'BUTTON'){
+                    console.warn("pf-button-group的子元素应全为pf-button")
+                }
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .pf-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        > .pf-button{
+            border-radius: 0;
+            &:not(:first-child){
+                margin-left:-1px;
+            }
+            &:first-child{
+                border-bottom-left-radius: var(--border-radius);
+                border-top-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-bottom-right-radius: var(--border-radius);
+                border-top-right-radius: var(--border-radius);
+            }
+            &:hover{
+                z-index: 1;
+                position:relative;
+            }
+        }
+    }
+</style>
