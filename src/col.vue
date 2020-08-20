ac7e82e10bde72380d891cedfa2497ae7315c80d
polly-foz <polly.foz@gmail.com>
2020-08-20 17:50:41 +0800

    col和row组件：添加gutter属性

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="col" :class="[`col-${span}`,`offset-${offset}`]">
-        <slot></slot>
+    <div class="col" :class="[`col-${span}`,`offset-${offset}`]"
+    :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+        <div class="content">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
@@ -10,16 +13,19 @@
         props:{
             span:{type:[Number,String]},
             offset:{type:[Number,String]},
-        }
+        },
+        data(){
+            return {
+                gutter:0
+            }
+        },
     };
 </script>
 
 <style lang="scss" scoped>
     .col{
         height: 100px;
-        background: grey;
         width: 50%;
-        border: 1px solid red;
     }
 
     $class-prefix: col-;
@@ -33,5 +39,10 @@
         .#{$class-prefix}#{$n} {
             margin-left: ($n/24)*100%;
         }
+    }
+
+    .content{
+        border: 1px solid blue;
+        height: 100%;
     }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,15 +1,25 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:'-'+gutter/2+'px',marginRight:'-'+gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: "PFRow"
+        name: "PFRow",
+        props:{
+            gutter:[String,Number]
+        },
+        mounted(){
+            this.$children.forEach(vm=>{
+                vm.gutter = this.gutter
+            })
+        }
     };
 </script>
 
 <style lang="scss" scoped>
-    .row{display:flex;}
+    .row{
+        display:flex;
+    }
 </style>
