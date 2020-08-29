2c0f29f6b9e92391822521e311e9d9d1d87d4faa
polly-foz <polly.foz@gmail.com>
2020-08-29 19:37:07 +0800

    Popover: 完成显示/关闭功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,40 +1,54 @@
 <template>
-    <div class="popover">
-        <div class="content" v-if="visible">
+    <div class="popover" @click.stop="onClickTrigger">
+        <div class="content" v-if="visible" @click.stop>
             <slot name="content"></slot>
         </div>
-        <div class="trigger" @click="onClickTrigger">
-            <slot></slot>
-        </div>
+        <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
         name: "PFPopover",
-        data(){
+        data() {
             return {
                 visible: false
-            }
+            };
         },
-        methods:{
-            onClickTrigger(){
-                this.visible = !this.visible
+        methods: {
+            onClickTrigger() {
+                // console.log('click');
+                this.visible = !this.visible;
+                const documentClickEventListener = () => {
+                    this.visible = false;
+                    // console.log('document隐藏popover');
+                    document.removeEventListener('click', documentClickEventListener);
+                    // console.log('doc删除监听器');
+                };
+                if (this.visible) {
+                    document.addEventListener('click', documentClickEventListener);
+                    // console.log('添加监听器');
+                } else {
+                    //TODO: 不影响使用，但下面这行代码没有起作用。。。
+                    document.removeEventListener('click', documentClickEventListener);
+                    // console.log('vm删除监听器');
+                    // console.log('vm隐藏popover');
+                }
+
             }
         }
     };
 </script>
 
 <style lang="scss" scoped>
-    .popover{
+    .popover {
         display: inline-block;
-        position:relative;
-        .content{
+        position: relative;
+        .content {
             position: absolute;
-            bottom:100%;
-            box-shadow: 0 0 1px rgba(0,0,0,0.5);
+            bottom: 100%;
+            box-shadow: 0 0 1px rgba(0, 0, 0, 0.5);
         }
-        .trigger{
-        }
+
     }
 </style>
