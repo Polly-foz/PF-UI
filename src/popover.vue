5bb0e9b2eb7bba7d84dd6cb44768c7301ae6fdfe
polly-foz <polly.foz@gmail.com>
2020-09-01 16:09:02 +0800

    Popover: 实现 click, hover 2种触发方式

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="popover" @click="onClick" ref="popover">
+    <div class="popover" ref="popover">
         <div class="contentWrapper" v-if="visible" ref="contentWrapper"
              :class="position && `position-${position}`">
             <slot name="content"></slot>
@@ -21,9 +21,32 @@
             position: {
                 type: String,
                 default: 'top',
-                validator(value){
-                    return ['top','left','right','bottom'].indexOf(value)>=0
+                validator(value) {
+                    return ['top', 'left', 'right', 'bottom'].indexOf(value) >= 0;
                 }
+            },
+            trigger: {
+                type: String,
+                default: 'click',
+                validator(value) {
+                    return ['click', 'hover'].indexOf(value) >= 0;
+                }
+            }
+        },
+        mounted() {
+            if(this.trigger === 'click'){
+                this.$refs.popover.addEventListener('click',this.onClick)
+            }else{
+                this.$refs.popover.addEventListener('mouseenter',this.open)
+                this.$refs.popover.addEventListener('mouseleave',this.close)
+            }
+        },
+        destroyed() {
+            if(this.trigger === 'click'){
+                this.$refs.popover.removeEventListener('click',this.onClick)
+            }else{
+                this.$refs.popover.removeEventListener('mouseenter',this.open)
+                this.$refs.popover.removeEventListener('mouseleave',this.close)
             }
         },
         methods: {
