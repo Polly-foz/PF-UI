84a971223d102c22476f7ae42340946a3ea1487c
polly-foz <polly.foz@gmail.com>
2020-09-02 06:20:12 +0800

    Popover: 实现部分测试用例

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ import Header from './src/header'
 import Sider from "./src/sider";
 import Content from "./src/content";
 import Footer from "./src/footer";
-export {Button,ButtonGroup,Icon,Input,Row,Col,Header,Sider,Content,Footer}
+import plugin from './plugin'
+import Toast from './toast'
+import Tabs from './tabs'
+import TabsHead from './tabs-head'
+import TabsItem from './tabs-item'
+import TabsBody from './tabs-body'
+import TabsPane from './tabs-pane'
+import Popover from './popover'
+export {Button,ButtonGroup,Icon,Input,Row,Col,Header,Sider,Content,Footer,plugin,Tabs,TabsHead,TabsItem,TabsBody,TabsPane,Popover}
 
