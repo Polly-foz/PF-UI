c02889b6fe66292f13a90b723682a2c3feb5fdf6
polly-foz <polly.foz@gmail.com>
2020-09-02 17:35:00 +0800

    Collapse: 支持single选项

--- src/collapse.vue
+++ src/collapse.vue
@@ -5,41 +5,55 @@
 </template>
 
 <script>
-    import Vue from 'vue'
+    import Vue from 'vue';
+
     export default {
         name: "PFCollapse",
-        props:{
-          selected:{
-              type: Array,
-              default: []
-          }
+        props: {
+            selected: {
+                type: Array,
+                default: []
+            },
+            single:{
+                type: Boolean,
+                default: false
+            }
         },
-        data(){
+        data() {
             return {
                 eventBus: new Vue()
-            }
+            };
         },
-        provide(){
-            return{
-                'eventBus':this.eventBus
-            }
+        provide() {
+            return {
+                'eventBus': this.eventBus
+            };
         },
-        mounted(){
-            this.eventBus.$emit('update:selected',this.selected)
-            this.eventBus.$on('add:selected',(name)=>{
-                let newSelected = JSON.parse(JSON.stringify((this.selected)))
-                newSelected.push(name)
-                this.eventBus.$emit('update:selected',newSelected)
-                this.$emit('update:selected',newSelected)
-
-            })
-            this.eventBus.$on('remove:selected',(name)=>{
-                let newSelected = JSON.parse(JSON.stringify((this.selected)))
-                const index = newSelected.indexOf(name)
-                newSelected.splice(index,1)
-                this.eventBus.$emit('update:selected',newSelected)
-                this.$emit('update:selected',newSelected)
-            })
+        mounted() {
+            this.eventBus.$emit('update:selected', this.selected);
+            this.eventBus.$on('add:selected', (name) => {
+                if(this.single){
+                    this.eventBus.$emit('update:selected', [name]);
+                    this.$emit('update:selected', [name]);
+                }else{
+                    let newSelected = JSON.parse(JSON.stringify((this.selected)));
+                    newSelected.push(name);
+                    this.eventBus.$emit('update:selected', newSelected);
+                    this.$emit('update:selected', newSelected);
+                }
+            });
+            this.eventBus.$on('remove:selected', (name) => {
+                if(this.single){
+                    this.eventBus.$emit('update:selected', []);
+                    this.$emit('update:selected', []);
+                }else{
+                    let newSelected = JSON.parse(JSON.stringify((this.selected)));
+                    const index = newSelected.indexOf(name);
+                    newSelected.splice(index, 1);
+                    this.eventBus.$emit('update:selected', newSelected);
+                    this.$emit('update:selected', newSelected);
+                }
+            });
         }
     };
 </script>
