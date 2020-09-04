4136396c3b74b704ba8508891d3b00d3d3abc2f9
polly-foz <polly.foz@gmail.com>
2020-09-04 09:26:15 +0800

    更新col组件

--- src/col.vue
+++ src/col.vue
@@ -1,151 +1,146 @@
 <template>
     <div class="col" :class="colClass"
-    :style="colStyle">
-        <div class="content">
-            <slot></slot>
-        </div>
+         :style="colStyle">
+        <slot></slot>
     </div>
 </template>
 
 <script>
-    const validator=(value)=>{
-        const keys = Object.keys(value)
-        let valid = true
-        keys.forEach(key=>{
-            if(!['span','offset'].includes(key)){
-                valid = false
+    const validator = (value) => {
+        const keys = Object.keys(value);
+        let valid = true;
+        keys.forEach(key => {
+            if (!['span', 'offset'].includes(key)) {
+                valid = false;
             }
-        })
-        return valid
-    }
+        });
+        return valid;
+    };
     export default {
         name: "PFCol",
-        data(){
+        data() {
             return {
-                gutter:0,
+                gutter: 0,
+            };
+        },
+        props: {
+            span: {type: [Number, String]},
+            offset: {type: [Number, String]},
+            pad: {type: Object, validator},
+            narrowPc: {type: Object, validator},
+            pc: {type: Object, validator},
+            widePc: {type: Object, validator}
+        },
+        methods: {
+            createClasses: (propsObject, devicePrefix = '') => {
+                if (!propsObject) {
+                    return [];
+                }
+                const {span, offset} = propsObject;
+                let arr = [];
+                if (span) {
+                    arr.push(`span-${devicePrefix}${span}`);
+                }
+                if (offset) {
+                    arr.push(`offset-${devicePrefix}${offset}`);
+                }
+                return arr;
             }
         },
-        props:{
-            span:{type:[Number,String]},
-            offset:{type:[Number,String]},
-            pad:{type:Object, validator},
-            narrowPc:{type:Object, validator},
-            pc:{type:Object, validator},
-            widePc:{type:Object, validator}
-        },
-        methods:{
-            createClasses:(propsObject,devicePrefix='')=>{
-                if(!propsObject){
-                    return []
-                }
-                const {span,offset} = propsObject
-                let arr = []
-                if(span){
-                    arr.push(`span-${devicePrefix}${span}`)
-                }
-                if(offset){
-                    arr.push(`offset-${devicePrefix}${offset}`)
-                }
-                return arr
-            }
-        },
-        computed:{
-            colClass(){
-                const {span,offset,pad,narrowPc,pc,widePc,createClasses} = this
+        computed: {
+            colClass() {
+                const {span, offset, pad, narrowPc, pc, widePc, createClasses} = this;
                 return [
-                    ...createClasses({span,offset}),
-                    ...createClasses(pad,'pad-'),
-                    ...createClasses(narrowPc,'narrowPc-'),
-                    ...createClasses(pc,'pc-'),
-                    ...createClasses(widePc,'widePc-')
-                ]
+                    ...createClasses({span, offset}),
+                    ...createClasses(pad, 'pad-'),
+                    ...createClasses(narrowPc, 'narrowPc-'),
+                    ...createClasses(pc, 'pc-'),
+                    ...createClasses(widePc, 'widePc-')
+                ];
             },
-            colStyle(){return {paddingLeft:this.gutter/2+'px',paddingRight:this.gutter/2+'px'}}
+            colStyle() {
+                return {paddingLeft: this.gutter / 2 + 'px', paddingRight: this.gutter / 2 + 'px'};
+            }
         }
     };
 </script>
 
 <style lang="scss" scoped>
-    .col{
-        height: 100px;
-        /*width: 50%;*/
-    }
-
-    $class-prefix: span-;
-    @for $n from 1 through 24 {
-        .#{$class-prefix}#{$n} {
-            width: ($n/24)*100%;
-        }
-    }
-    $class-prefix: offset-;
-    @for $n from 1 through 24 {
-        .#{$class-prefix}#{$n} {
-            margin-left: ($n/24)*100%;
-        }
-    }
-
-    @media (min-width:577px) {
-        $class-prefix: span-pad-;
+    .col {
+        $class-prefix: span-;
         @for $n from 1 through 24 {
-            .#{$class-prefix}#{$n} {
+            &.#{$class-prefix}#{$n} {
                 width: ($n/24)*100%;
             }
         }
-        $class-prefix: offset-pad-;
+
+        $class-prefix: offset-;
         @for $n from 1 through 24 {
-            .#{$class-prefix}#{$n} {
+            &.#{$class-prefix}#{$n} {
                 margin-left: ($n/24)*100%;
+            }
+        }
+
+        @media (min-width: 577px) {
+            $class-prefix: span-pad-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n/24)*100%;
+                }
+            }
+            $class-prefix: offset-pad-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    margin-left: ($n/24)*100%;
+                }
+            }
+        }
+
+        @media (min-width: 769px) {
+            $class-prefix: span-narrowPc-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n/24)*100%;
+                }
+            }
+            $class-prefix: offset-narrowPc-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    margin-left: ($n/24)*100%;
+                }
+            }
+        }
+
+        @media (min-width: 993px) {
+            $class-prefix: span-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n/24)*100%;
+                }
+            }
+            $class-prefix: offset-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    margin-left: ($n/24)*100%;
+                }
+            }
+        }
+
+        @media (min-width: 1201px) {
+            $class-prefix: span-widePc-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n/24)*100%;
+                }
+            }
+            $class-prefix: offset-widePc-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    margin-left: ($n/24)*100%;
+                }
             }
         }
     }
 
-    @media (min-width:769px) {
-        $class-prefix: span-narrowPc-;
-        @for $n from 1 through 24 {
-            .#{$class-prefix}#{$n} {
-                width: ($n/24)*100%;
-            }
-        }
-        $class-prefix: offset-narrowPc-;
-        @for $n from 1 through 24 {
-            .#{$class-prefix}#{$n} {
-                margin-left: ($n/24)*100%;
-            }
-        }
-    }
 
-    @media (min-width:993px) {
-        $class-prefix: span-pc-;
-        @for $n from 1 through 24 {
-            .#{$class-prefix}#{$n} {
-                width: ($n/24)*100%;
-            }
-        }
-        $class-prefix: offset-pc-;
-        @for $n from 1 through 24 {
-            .#{$class-prefix}#{$n} {
-                margin-left: ($n/24)*100%;
-            }
-        }
-    }
-
-    @media (min-width:1201px) {
-        $class-prefix: span-widePc-;
-        @for $n from 1 through 24 {
-            .#{$class-prefix}#{$n} {
-                width: ($n/24)*100%;
-            }
-        }
-        $class-prefix: offset-widePc-;
-        @for $n from 1 through 24 {
-            .#{$class-prefix}#{$n} {
-                margin-left: ($n/24)*100%;
-            }
-        }
-    }
-
-    .content{
-        border: 1px solid blue;
-        height: 100%;
-    }
 </style>
